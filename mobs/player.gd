d8f0b23ac3c8f131b2953c9794c50f647bc06b74
callerfft <callerff01@gmail.com>
2026-02-04 15:02:34 +0500

    Update player.gd

--- mobs/player.gd
+++ mobs/player.gd
@@ -89,6 +89,9 @@ func _ready():
 	death_screen.visible = false
 	pause_button.visible = true
 	timer.start()
+	GameManager.upgrades.turret_count = 5
+	GameManager.emit_stats()
+	print("🎁 Given 5 turrets for testing")
 	
 	# Инициализируем HP из GameManager
 	health_bar.init_health(hp)
