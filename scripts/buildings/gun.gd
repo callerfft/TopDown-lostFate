d384b1a8376dfa49c5d22760ff198282f01ac9fd
yohanson <callerff01@gmail.com>
2026-03-28 02:01:13 +0600

    init

---
+++ scripts/buildings/gun.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@onready var muzzle: Marker2D = $Marker2D
+
+const BULLET = preload("res://scenes/buildings/ammo.tscn")
+
+func _process(_delta: float) -> void:
+	look_at(get_global_mouse_position())
+	rotation_degrees = wrap(rotation_degrees,0, 360)
+	if rotation_degrees > 90 and rotation_degrees < 270:
+		scale.y = -1
+	else:
+		scale.y = 1
+	if Input.is_action_just_pressed("attack"):
+		$"../sounds/shoot".play()
+		var bullet_instance = BULLET.instantiate()
+		get_tree().root.add_child(bullet_instance)
+		bullet_instance.global_position = muzzle.global_position
+		bullet_instance.rotation = rotation
+ 
